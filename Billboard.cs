ace6310556d4fdd56795b6d1c26d071da36e381f
s3cs <65388207+s3cs@users.noreply.github.com>
2020-06-19 10:09:58 +0200

    Update Billboard.cs

--- Billboard.cs
+++ Billboard.cs
@@ -7,14 +7,6 @@ public class Billboard : MonoBehaviour
 
     public GameObject cam;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         transform.LookAt(cam.transform.position, -Vector3.forward);   
